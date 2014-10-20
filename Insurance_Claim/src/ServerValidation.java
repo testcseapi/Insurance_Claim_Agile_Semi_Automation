9479bb27f44e1171c5cbccbba0206cb6f6728312
testcseapi <testcseapi@gmail.com>
2014-10-20 19:30:59 +0530

    updated

--- Insurance_Claim/src/ServerValidation.java
+++ Insurance_Claim/src/ServerValidation.java
@@ -56,7 +56,7 @@ public class ServerValidation extends HttpServlet {
 		String Alphone = request.getParameter("Alphone");
 		String datepicker = request.getParameter("datepicker");
 		String Ltime = request.getParameter("Ltime");
-//		String Ldesc = request.getParameter("Ldesc");
+		// String Ldesc = request.getParameter("Ldesc");
 		String Laddress = request.getParameter("Laddress");
 		String Lcity = request.getParameter("Lcity");
 		String Lzipcode = request.getParameter("Lzipcode");
@@ -64,32 +64,25 @@ public class ServerValidation extends HttpServlet {
 		String LCountry = request.getParameter("LCountry");
 
 		String error = "";
-	//	String error = null;
+		// String error = null;
 		boolean result = true;
 
-   		if (ClaimSubmit.equals("")) {
+		if (ClaimSubmit == "") {
+			// if (ClaimSubmit.equals("")) {
 			error += "Please Select the ClaimSubmit<br/>";
 			result = false;
 
 		}
-		
-//		if (ClaimSubmit=="") {
-//			error += "Please Select the ClaimSubmit<br/>";
-//			result = false;
-//
-//		}
 
-		
 		if (Fname.equals("")) {
 			error += "Please enter Firstname<br/>";
 			result = false;
 		}
 		/*
-		if (Fname=="") {
-			error += "Please enter Firstname<br/>";
-			result = false;
-		}*/
-		
+		 * if (Fname=="") { error += "Please enter Firstname<br/>"; result =
+		 * false; }
+		 */
+
 		if (Lname.equals("")) {
 			error += "Please enter Lastname<br/>";
 			result = false;
@@ -141,11 +134,11 @@ public class ServerValidation extends HttpServlet {
 			error += "Please enter the LossTime<br/>";
 			result = false;
 		}
-//		if (Ldesc.equals("")) {
-//			error += "Please Describe the Loss Description<br/>";
-//			result = false;
-//		}
-//
+		// if (Ldesc.equals("")) {
+		// error += "Please Describe the Loss Description<br/>";
+		// result = false;
+		// }
+		//
 		if (LState.equals("")) {
 			error += "Please select the LossState<br/>";
 			result = false;
@@ -175,7 +168,7 @@ public class ServerValidation extends HttpServlet {
 			request.setAttribute("Wphone", Wphone);
 			request.setAttribute("Cphone", Cphone);
 			request.setAttribute("Alphone", Alphone);
-//			request.setAttribute("Ldesc", Ldesc);
+			// request.setAttribute("Ldesc", Ldesc);
 			request.setAttribute("Laddress", Laddress);
 			request.setAttribute("Lcity", Lcity);
 			request.setAttribute("Lzipcode", Lzipcode);
