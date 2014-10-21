f0763a2e11740a77cad3abbc1b44abc95ab49418
testcseapi <testcseapi@gmail.com>
2014-10-21 16:05:23 +0530

    Selenium

--- Insurance_Claim/TestCase/DevTest.java
+++ Insurance_Claim/TestCase/DevTest.java
@@ -84,7 +84,7 @@ public class DevTest extends TestCase {
 			driver.findElement(By.id("submit")).submit();
 			Thread.sleep(5000);
 
-			/*String error = driver.findElement(By.id("error")).getAttribute(
+			String error = driver.findElement(By.id("error")).getAttribute(
 					"value");
 			if (error.isEmpty()) {
 				System.out.println("Selenium Test Passed");
@@ -92,7 +92,7 @@ public class DevTest extends TestCase {
 				System.out.println("Selenium Test Failed");
 				System.out.println(error);
 				Assert.fail("Selenium Test Failed   " + error);
-			}*/
+			}
 
 			 System.out.println("Selenium Test Passed");
 
